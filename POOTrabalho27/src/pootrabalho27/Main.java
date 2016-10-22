8ebef27e950a26699d7828067f687a269212c2ac
vitao375 <vitao375@10.2.14.251>
2016-10-22 13:28:12 -0200

    @vitao375 Algumas edições aleatórias

--- POOTrabalho27/src/pootrabalho27/Main.java
+++ POOTrabalho27/src/pootrabalho27/Main.java
@@ -9,13 +9,14 @@ package pootrabalho27;
  *
  * @author vitao375
  */
-public class POOTrabalho27 {
+public class Main {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       //main
+       FrameView abrir = new FrameView();
+       abrir.setVisible(true);
     }
     
 }
